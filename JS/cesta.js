4e8a8a1ae839af023bd9d0ca9461b2ae32c96c4a
jjulissa <julissajoseph9@gmail.com>
2023-01-11 15:38:17 +0100

    cesta

--- JS/cesta.js
+++ JS/cesta.js
@@ -2,6 +2,9 @@
 let cesta = JSON.parse(localStorage.getItem("caja")); 
 // console.log(cesta); 
 
+let prixJs = document.querySelector("#prixJs"); 
+let totalJs = document.querySelector("#totalJs"); 
+
 cesta.forEach(element => { 
     let cestaTres = document.querySelector(".cestaTres");  
     let CadaCesta = document.createElement("div"); 
@@ -9,12 +12,18 @@ cesta.forEach(element => {
     
     let masoMenosxs = document.createElement("div"); 
     masoMenosxs.classList.add("masoMenosxs"); 
+
     let menos = document.createElement("img"); 
-    menos.src = "../asset/icono/menos.png"
+    menos.src = "../asset/icono/men.png"; 
+    menos.classList.add("menos"); 
+
     let cantidad = document.createElement("p"); 
     cantidad.innerText = 1; 
+    cantidad.classList.add("cantidad")
+
     let mas = document.createElement("img"); 
-    mas.src = "../asset/icono/mas.png"
+    mas.src = "../asset/icono/mass.png"; 
+    mas.classList.add("mas");
     
     let img = document.createElement("img"); 
     img.classList.add("fotoCafe")
@@ -38,9 +47,14 @@ cesta.forEach(element => {
     titCesta.append(titulo, descripcion); 
     cestaTres.append(CadaCesta, hrmedio); 
 
+  
 
 
-    
+        // Suma 
+
+prixJs.innerText = (parseInt(prixJs.innerText) + parseInt(element.price)) + "€";   
+
+totalJs.innerText = parseInt(prixJs.innerText) + "€"; 
 
 
 }); 
